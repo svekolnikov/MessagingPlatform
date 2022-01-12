a5da59b5a75956c7c78ae2b502c1db355126cf40
Alexey <alexey.svekolnikov@gmail.com>
2022-01-12 21:03:36 +0300

    init

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern:"{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
