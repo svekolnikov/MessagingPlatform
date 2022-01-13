12dc9827741f2abe21735dde87f8e9f5055c3adb
Alexey <alexey.svekolnikov@gmail.com>
2022-01-13 21:55:43 +0300

    changed structure of solution. Common, Services, UI, Data

---
+++ Common/MessagingPlatform.Domain/Entities/Base/Entity.cs
@@ -0,0 +1,9 @@
+﻿using MessagingPlatform.Interfaces.Base.Entities;
+
+namespace MessagingPlatform.Domain.Entities.Base
+{
+    public class Entity : IEntity
+    {
+        public int Id { get; set; }
+    }
+}
