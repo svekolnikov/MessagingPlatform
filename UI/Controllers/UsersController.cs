49dffa174a879739bafc7042345fbabac0e49ce5
Alexey <alexey.svekolnikov@gmail.com>
2022-01-14 20:57:23 +0300

    Add action Create User

---
+++ UI/Controllers/UsersController.cs
@@ -0,0 +1,69 @@
+﻿using System.Runtime.CompilerServices;
+using AutoMapper;
+using MessagingPlatform.Domain.Entities;
+using MessagingPlatform.Interfaces;
+using MessagingPlatform.MVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MessagingPlatform.MVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ILogger<UsersController> _logger;
+        private readonly IUsersManager _usersManager;
+        private readonly IMapper _mapper;
+
+        public UsersController(ILogger<UsersController> logger, IUsersManager usersManager, IMapper mapper)
+        {
+            _logger = logger;
+            _usersManager = usersManager;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await _usersManager.Users.GetAllAsync();
+            var userViewModels = _mapper.Map<List<UserViewModel>>(users);
+
+            return View(userViewModels);
+        }
+
+        public IActionResult Create() => View(new UserViewModel());
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(UserViewModel userViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(userViewModel);
+
+            try
+            {
+                var user = _mapper.Map<User>(userViewModel);
+                await _usersManager.Users.AddAsync(user);
+            }
+            catch (Exception e)
+            {
+                LogError(e);
+                throw;
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return View(new UserViewModel());
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        private void LogError(Exception e, [CallerMemberName] string methodName = null!)
+        {
+            _logger.LogError(e, "Error: {0}", methodName);
+        }
+    }
+}
